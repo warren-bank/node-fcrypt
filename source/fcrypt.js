f526cae5e53410d2cbcc888f3445be9ef08a9b78
Warren R Bank <warren.r.bank@gmail.com>
2020-01-27 17:31:38 -0800

    v2.1.0: refactor to remove redundant code

--- source/fcrypt.js
+++ source/fcrypt.js
@@ -55,11 +55,13 @@ function encrypt(param) {
     var encrypt = crypto.createCipher(param.method, param.key);
   }
   catch (err) {
+    if (err.message === "Unknown cipher")
+      errors.addError("Incorrect method", 150);
+    else if (err.message === "Key must be a buffer")
+      errors.addError("Incorrect key", 151);
+    else
+      errors.addError("Crypto error", 152);
 
-    if (err.message === "Unknown cipher") errors.addError("Incorrect method", 150);
-    else if (err.message === "Key must be a buffer") errors.addError("Incorrect key", 151);
-    else errors.addError("Crypto error", 152);
-    
     param.callback(errors);
     return;
   }
@@ -85,19 +87,29 @@ function encrypt(param) {
       zipfile.addEmptyDirectory(subpath);
       return;
     }
-    
+
     //File:
     zipfile.addReadStream(fs.createReadStream( itemPath ), subpath);
   }, () => zipfile.end() );
 }
 
-function decrypt(param) {
+function abstract_decrypter(extract, param) {
   //include:
   //  param.key
   //  param.input
   //  param.output
   //  param.callback
   //  param.method
+
+  var output_dir = extract
+    ? param.output
+    : path.dirname(param.output)
+  ;
+
+  var output_pipe = extract
+    ? unzipper.Extract({path: param.output})
+    : fs.createWriteStream(param.output)
+  ;
 
   var errors = new Errors();
 
@@ -111,28 +123,29 @@ function decrypt(param) {
     return;
   }
 
-  //create output folder, if it doesn't exist
-  makedir(path.dirname(param.output));
+  //create output directory, if it doesn't exist
+  makedir(output_dir);
 
   //create decrypter pipe
   try {
     var decrypt = crypto.createDecipher(param.method, param.key);
   }
   catch (err) {
+    if (err.message === "Unknown cipher")
+      errors.addError("Incorrect method", 150);
+    else if (err.message === "Key must be a buffer")
+      errors.addError("Incorrect key", 151);
+    else
+      errors.addError("Crypto error", 152);
 
-    if (err.message === "Unknown cipher") errors.addError("Incorrect method", 150);
-    else if (err.message === "Key must be a buffer") errors.addError("Incorrect key", 151);
-    else errors.addError("Crypto error", 152);
-    
     param.callback(errors);
     return;
   }
 
-  //Decrypt only
   var readStream = fs.createReadStream(param.input);
   readStream
     .pipe(decrypt)
-    .pipe(fs.createWriteStream( param.output ))
+    .pipe(output_pipe)
     .on("error", (err) => {
       readStream.destroy();
       errors.addError("Key is incorrect", 303);
@@ -144,57 +157,14 @@ function decrypt(param) {
   ;
 }
 
+function decrypt(param) {
+  var extract = false;
+  abstract_decrypter(extract, param);
+}
+
 function extract(param) {
-  //include:
-  //  param.key
-  //  param.input
-  //  param.output
-  //  param.callback
-  //  param.method
-
-  var errors = new Errors();
-
-  //Default
-  param.method = param.method || "aes-256-cbc";
-
-  //Does input file exist?
-  if (!fs.existsSync(param.input)) {
-    errors.addError("Input file doesn't exist", 101);
-    param.callback(errors);
-    return;
-  }
-
-  //create output folder, if it doesn't exist
-  makedir(param.output);
-
-  //create decrypter pipe
-  try {
-    var decrypt = crypto.createDecipher(param.method, param.key);
-  }
-  catch (err) {
-
-    if (err.message === "Unknown cipher") errors.addError("Incorrect method", 150);
-    else if (err.message === "Key must be a buffer") errors.addError("Incorrect key", 151);
-    else errors.addError("Crypto error", 152);
-    
-    param.callback(errors);
-    return;
-  }
-
-  //Decrypt and uncompress
-  var readStream = fs.createReadStream(param.input);
-  readStream
-    .pipe(decrypt)
-    .pipe(unzipper.Extract({ path: param.output }))
-    .on("error", (err) => {
-      readStream.destroy();
-      errors.addError("Key is incorrect", 303);
-      param.callback(errors);
-    })
-    .on("close", () => {
-      param.callback(errors);
-    });
-  ;
+  var extract = true;
+  abstract_decrypter(extract, param);
 }
 
 module.exports = {
