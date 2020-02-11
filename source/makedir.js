72e4dcc6a8f37f90ee22df6185e382e911cbcd57
Warren R Bank <warren.r.bank@gmail.com>
2020-02-10 16:47:21 -0800

    v2.1.1: fix recursive mkdir for posix paths

--- source/makedir.js
+++ source/makedir.js
@@ -1,24 +1,28 @@
-var path = require("path");
-var fs = require("fs");
+const path = require('path')
+const fs   = require('fs')
 
-// copied from:
-//   https://github.com/developersworkspace/DirVault/blob/b9cf877a53576307301c293924baa999e9a89359/src/app.js#L94
-// MIT license:
-//   https://github.com/developersworkspace/DirVault/blob/b9cf877a53576307301c293924baa999e9a89359/LICENSE
+const createDirectory = (dirpath) => {
+  if (!dirpath || (typeof dirpath !== 'string')) return
+  let parts
 
-function createDirectory(dirpath) {
-    let parts = dirpath.split(path.sep);
-    for (let i = 1; i <= parts.length; i++) {
-        mkdirSync(path.join.apply(null, parts.slice(0, i)));
-    }
+  parts = dirpath.split(path.sep)
+  parts = parts.filter(part => !!part)
+  if (dirpath.indexOf(path.sep) === 0) {
+    parts.unshift(path.sep)
+  }
+  for (let i = 1; i <= parts.length; i++) {
+    mkdirSync(path.join.apply(null, parts.slice(0, i)))
+  }
 }
 
-function mkdirSync(path) {
-    try {
-        fs.mkdirSync(path);
-    } catch (e) {
-        if (e.code != 'EEXIST') throw e;
-    }
+const mkdirSync = (dirpath) => {
+  if (dirpath === path.sep) return
+
+  try {
+    fs.mkdirSync(dirpath)
+  } catch (e) {
+    if (e.code !== 'EEXIST') throw e
+  }
 }
 
-module.exports = createDirectory;
+module.exports = createDirectory
